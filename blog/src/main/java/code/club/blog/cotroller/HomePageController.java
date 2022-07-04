1854d08b64f5f1c96f2284db06d774c935f24310
Ustymscorpion <Ustym.horin@gmail.com>
2022-07-04 21:47:09 +0200

    Change File

---
+++ blog/src/main/java/code/club/blog/cotroller/HomePageController.java
@@ -0,0 +1,26 @@
+package code.club.blog.cotroller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HomePageController {
+    @GetMapping
+    public String sayHello(){
+        return "hello";
+    }
+    @GetMapping("/bye")
+    public String sayBye(){
+        return "bye";
+    }
+    @GetMapping("/GraficCard")
+    public String sayTI(){
+        return "NVIDIA GeForce RTX 3080 ti";
+    }
+    @GetMapping("/GraficCard")
+    public String sayNotTI() {
+        return "NVIDIA GeForce RTX 3080";
+    }
+}
